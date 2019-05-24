a5726f89575435a3fc173a72d3e4a3b19a586fdc
Elena <37112748+elenasl96@users.noreply.github.com>
2019-05-24 15:40:34 +0200

    edit project with transform

--- project/src/pub.cpp
+++ project/src/pub.cpp
@@ -10,40 +10,32 @@ using namespace message_filters;
 class car_sub_pub
 {
 	public:
-	car_sub_pub(){	
-	}
-
-	void subscribe(){
-
+	car_sub_pub(){
 		//subsribe to multiple topics with message_filters
 		message_filters::Subscriber<first_project::floatStamped> speedL_sub(n, "speed_L_stamped", 1);
 		message_filters::Subscriber<first_project::floatStamped> speedR_sub(n, "speed_R_stamped", 1);
 		message_filters::Subscriber<first_project::floatStamped> steer_sub(n, "steer_stamped", 1);
-
  		TimeSynchronizer<first_project::floatStamped, first_project::floatStamped> sync(speedL_sub, speedR_sub, 10);
- 		sync.registerCallback(boost::bind(callback, _1, _2));
 	}
 
 
-	void static callback(const first_project::floatStamped::ConstPtr& , const first_project::floatStamped::ConstPtr& ){
- 
-		//create the tfBroadcaster -> this will send the transformations 
+	void callback(const first_project::floatStamped::ConstPtr& , const first_project::floatStamped::ConstPtr& ){
+		//create the tfBroadcaster -> this will send the transformations
 		tf::TransformBroadcaster br;
 		//create the transform object
 		tf::Transform transform;
 		transform.setOrigin( tf::Vector3(0, 0, 0) );
 		tf::Quaternion q;
-		q.setRPY(0, 0, 0);			
+		q.setRPY(0, 0, 0);
 		transform.setRotation(q);
 
 		//send the transformation (transform, time, "parentFrame", "childFrame")
-		br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "turtle")); 
-		}
+		br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "turtle"));
+	}
 
-	public:
-	//create the subscriber
+	private:
+	//create the NodeHandle
 	ros::NodeHandle n;
-	ros::Subscriber sub;
 	};
 
 
@@ -51,7 +43,7 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "subscribe_and_publish");
 	car_sub_pub my_car_sub_pub;
+	sync.registerCallback(boost::bind(callback, _1, _2));
 	ros::spin();
 	return 0;
 }
-
