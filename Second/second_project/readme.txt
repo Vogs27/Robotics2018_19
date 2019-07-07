6df9c98a70caaed06b8164f138fb07a4b3bc9b37
Alessandro Vogrig <alessandroandrea.vogrig@mail.polimi.it>
2019-07-07 23:53:17 +0200

    Potrebbe essere finito
    Readme fatto, odom pub fatto, nav etc fatto
    Si avvia, ma ho paura a vedere se stampa giusto o meno

---
+++ Second/second_project/readme.txt
@@ -0,0 +1,41 @@
+﻿Team members:
+- Alessandro Andrea Vogrig (matr. 845461 - cod pers. 10527163)
+- Elena Rosalba Schinelli (matr. 844329 - cod pers. 10501436)
+________________________________________________________________________________
+Package name: second_project
+Node name: odom_pub
+________________________________________________________________________________
+Package structure:
+first_project
+├── include
+│   └── imu_complementary_filter
+|       ├── complementary_filter_ros.h
+|       └── complementary_filter.h
+├── src
+│   ├── odom_pub.cpp
+│   ├── complementary_filter.cpp
+│   ├── complementary_filter_node.cpp
+|   └── complementary_filter_ros.cpp
+├── launch
+│   └── launch_all.launch
+├── params
+│   └── navsat_transform_param.yaml
+├── CMakeLists.txt
+├── package.xml
+├── project.bag
+└── readme.txt
+________________________________________________________________________________
+File description:
+- odom_pub.cpp: our main node. Compute odometry from "speedsteer" topic and publishes under "myTopic/odom"
+- launch_all.launch: launch file to launch odom_pub, imu_complementary_filter (cpp files included in the package) and robot localization (sources not included).
+- complementary_filter files: files to realize imu complementaty message_filter
+- navsat_transform_param.yaml: based on prof. Mentasti's, considering the same source of data, we have used a similar config file for navsat node.
+________________________________________________________________________________
+How to run our project:
+1. Make sure you have installed all dipendencies (imu_tools and robot_localization)
+2. Make sure you have the package "robot_localization" compiled and avaiable.
+3. Place the package folder in your ROS workspace and compile it with catkin_make
+4. Run command "source devel/setup.bash" in your workspace root
+5. Run roscore and run the bag file in a separate terminal with rosbag play -l --clock project.bag
+6. Start our project with the launch file. roslaunch second_project launch_all.launch
+________________________________________________________________________________
--- Second/second_project/src/odom_pub.cpp
+++ Second/second_project/src/odom_pub.cpp
@@ -2,46 +2,85 @@
 #include <cmath>
 #include <string>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 
 #define _USE_MATH_DEFINES
-/*
+
 class odom_pub{
 
 public:
 	odom_pub(){
-		ros::Subscriber sub = n.subscribe("speedster", 1000, callback);//subscribe to telemetry topic
-
-		odom_pub = n.advertise<nav_msgs::Odometry>("world", 50); //create odometry topic Publisher
+		sub = n.subscribe("/speedsteer", 1, &odom_pub::callbackTel, this);//subscribe to telemetry topic
+		odom_publisher = n.advertise<nav_msgs::Odometry>("myTopic/odom", 1); //create odometry topic Publisher
 
 		current_time = ros::Time::now(); //take first sys time value for dt calculation
 		last_time = current_time; //last time is the same @ the first time
-		ros::spin();
 	}
 
 private:
 	ros::NodeHandle n; //node handler
-	ros::Publisher odom_pub; //odometry topic publisher
+	ros::Publisher odom_publisher; //odometry topic publisher
+	ros::Subscriber sub;
 	ros::Time current_time, last_time;
+	double x_ackermann = 0.0;
+	double y_ackermann = 0.0;
+	double th_ackermann = 0.0;
+	tf2::Quaternion quat_tf;
 
 
-	void callback(const first_project::floatStampedConstPtr& speedL, const first_project::floatStampedConstPtr& speedR, const first_project::floatStampedConstPtr& steer)  //callback for message filter
+void callbackTel(const geometry_msgs::PointStamped::ConstPtr& msg)
 	{
 		//compute dt (delta t)
 		current_time = ros::Time::now();
 		double dt = (current_time - last_time).toSec();
+		//retreive velocity and steering angle from msg
+		double Vel = msg -> point.x;
+		double steer = msg -> point.y;
+		double sA = steer / (double)18 * M_PI / (double)180;
 
-		//get velocities from bag's topics
-		float vL = speedL -> data;
-		float vR = speedR -> data;
-		float s = steer -> data;
+		double	w_ack = Vel * tan(sA) / 1.765;
+
+		double delta_th_ack = w_ack * dt;
+		th_ackermann += delta_th_ack;
+
+		if(th_ackermann > 2 * M_PI) th_ackermann -= 2 * M_PI;
+		if(th_ackermann < 0) th_ackermann += 2 * M_PI;
+
+		double v_ack = w_ack * 1.765 / sin (sA);
+		double delta_x_ack = v_ack * cos(th_ackermann) * dt;
+		double delta_y_ack = v_ack * sin(th_ackermann) * dt;
+		x_ackermann += delta_x_ack;
+		y_ackermann += delta_y_ack;
+		quat_tf.setRPY( 0, 0, th_ackermann );
+		geometry_msgs::Quaternion odom_quat_ack = tf2::toMsg(quat_tf);
+		nav_msgs::Odometry odom_ack;
+		odom_ack.header.stamp = current_time;
+		odom_ack.header.frame_id = "world";
+
+		//set the position
+		odom_ack.pose.pose.position.x = x_ackermann;
+		odom_ack.pose.pose.position.y = y_ackermann;
+		odom_ack.pose.pose.position.z = 0.0;
+		odom_ack.pose.pose.orientation = odom_quat_ack;
+
+		//set the velocity
+		odom_ack.child_frame_id = "base_link";
+		odom_ack.twist.twist.linear.x = v_ack * cos(th_ackermann);
+		odom_ack.twist.twist.linear.y = v_ack * sin(th_ackermann);
+		odom_ack.twist.twist.angular.z = w_ack;
+
+	}
 
 };
-*/
+
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "odom_pub");
-	//tf_odom_pub my_odom_calculator;
+	odom_pub my_odom_calculator;
 	ros::spin();
-
 	return 0;
 }
